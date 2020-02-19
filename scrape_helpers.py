cffeb461fdcca58a509396a5aeaa6117172dd736
Christopher Tiller <christiller93@protonmail.com>
2020-02-18 20:39:57 -0500

    Create fourth passing test case. Begin fixing functional tests

--- scrape_helpers.py
+++ scrape_helpers.py
@@ -6,16 +6,16 @@ def generate_request_url(site, query, location):
         return f'https://www.monster.com/jobs/search/?q={query}&where={location}'
 
 def parse_relevant_job_data(html, site, query, location):
-    print(html)
-    print(html[0:50])
     soup = BeautifulSoup(html, 'html.parser')
-    #print(soup.find('h2', class_='title'))
     title = soup.find('h2', class_='title').text.strip().lower()
-    location = soup.find('div', class_ = 'location').text.strip().lower()
+    location = soup.find('div', class_='location').text.strip().lower()
     link = soup.find('a')['href']
     return models.JobDataModel(title, location, link)
 
 def parse_job_list(html_list, site, query, location):
-    print(html_list)
-    job_data_list = [parse_relevant_job_data(html, site, query, location) for html in html_list]
+    job_data_list = [parse_relevant_job_data(str(html), site, query, location) for html in html_list]
     return job_data_list
+
+def parse_html_to_lists(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    return soup.find(id='ResultsContainer').findAll('section', class_='card-content')
